50decafd4a71ddf881b5bb0dac5f2bab93ee3f37
Lola <lia1m@aol.com>
2021-10-27 13:39:27 -0400

    editted savedbooks

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import { Container, Col, Form, Button, Card } from 'react-bootstrap';
+import { Jumbotron, CardColumns } from 'bootstrap';
 
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+
+import {SAVE_BOOK} from '../utils/mutations',
+import {useMutation} from '@apollo/react-hooks';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -13,6 +17,8 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  const [saveBook] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
